c4bb16ae5ea981150bf2a7cdf4338ddcad4f68c8
scepter914 <scepter914@gmail.com>
2022-09-02 14:01:06 +0900

    fix regular expression of remap_rosbag2
    Signed-off-by: scepter914 <scepter914@gmail.com>

--- remap_rosbag2/remap_rosbag2.sh
+++ remap_rosbag2/remap_rosbag2.sh
@@ -5,13 +5,20 @@ FILE_NAME=$@
 TOPIC_FILTER=""
 
 ### Record topic with regular expressions ###
-PERCEPTION_TOPIC=$(ros2 topic list | grep --regexp="/perception/object_recognition/*")
-TOPIC_FILTER="$TOPIC_FILTER $PERCEPTION_TOPIC"
+PERCEPTION_TOPIC=$(ros2 bag info $FILE_NAME | awk '{print $2}' | grep --regexp="/perception/*")
+
+for TOPIC in ${PERCEPTION_TOPIC[@]}; do
+  TOPIC_FILTER+=" "
+  TOPIC_FILTER+=$TOPIC
+  TOPIC_FILTER+=":=/original"
+  TOPIC_FILTER+=$TOPIC
+  TOPIC_FILTER+=" "
+done
 
 TOPIC_LIST=(
-/perception/object_recognition/detection/objects
-/perception/object_recognition/tracking/objects
-/perception/object_recognition/objects
+# /perception/object_recognition/detection/objects
+# /perception/object_recognition/tracking/objects
+# /perception/object_recognition/objects
 )
 
 for TOPIC in ${TOPIC_LIST[@]}; do
@@ -22,4 +29,6 @@ for TOPIC in ${TOPIC_LIST[@]}; do
   TOPIC_FILTER+=" "
 done
 
+echo $TOPIC_FILTER
 ros2 bag play $FILE_NAME --clock 200 -s sqlite3 --remap $TOPIC_FILTER
+
